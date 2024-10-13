179c577c9ef7d10bee901c55c5a9165f64458b84
sonjungchan <rnrmfjc@gmail.com>
2024-10-13 19:41:02 +0900

    init

---
+++ src/test/java/shareschedule/servergateway/ServerGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package shareschedule.servergateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
